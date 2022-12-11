6e05a8f0ece06fc4df06655fcea494772d9737b3
vandre_borba <vandretrevisan@gmail.com>
2022-12-11 19:54:39 +0000

    extra-tips

--- lib/new_buttons.dart
+++ lib/new_buttons.dart
@@ -1,7 +1,36 @@
+import 'package:flutter/material.dart';
+
 class SomeButton extends StatelessWidget {
-  const SomeButton({Key? key}) : super(key: key);
+  const SomeButton({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.orange),
+              foregroundColor: MaterialStateProperty.all(Colors.white)),
+          onPressed: null,
+          child: Text("A raised Button"),
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              primary: Colors.orange,
+              onPrimary: Colors.white), // onPrimary é a cor do texto.
+          onPressed: null, child: Text("A raised Button - 2"),
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+              primary: Colors.orange), // primary é a cor principal do botão.
+          onPressed: null, child: Text("A raised Button - 2"),
+        ),
+        OutlinedButton(
+          style: OutlinedButton.styleFrom(
+              primary: Colors.orange, side: BorderSide(color: Colors.amber)),
+          onPressed: null,
+          child: Text("A raised Button - 2"),
+        )
+      ],
+    );
   }
 }
